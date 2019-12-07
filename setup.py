be971709fb0698ce3db407c7326d7e58fa95ca82
zipy124 <iiianmmm@gmail.com>
2019-12-07 14:15:12 +0000

    Change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="uclapyi-uclapi", # Replace with your own username
+    name="uclapyi",
     version="0.0.1",
     author="UCLAPI",
     author_email="isd.apiteam@ucl.ac.uk",
