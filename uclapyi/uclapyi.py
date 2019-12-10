2d00b07f0b5a0898163a57d31f3f551616c0f998
zipy124 <iiianmmm@gmail.com>
2019-12-10 18:20:42 +0000

    Add most sensors endpoints

--- uclapyi/uclapyi.py
+++ uclapyi/uclapyi.py
@@ -1,6 +1,8 @@
 import requests
 import pytz
 import datetime
+from collections import namedtuple
+
 
 class BadResponseError(Exception):
     pass
@@ -14,6 +16,7 @@ class Client:
         self.roombookings = self.roombookings(self)
         self.search = self.search(self)
         self.resources = self.resources(self)
+        self.workspaces = self.workspaces(self)
 
     class roombookings:
 
@@ -102,6 +105,70 @@ class Client:
             for desktop in result["data"]:
                 desktops.append(Desktop(desktop))
             return desktops
+
+    class workspaces:
+
+        def __init__(self, client):
+            self.client = client
+            self.sensors = self.sensors(client)
+
+        def surveys(self, survey_filter="student"):
+            params = {}
+            params["token"] = self.client.token
+            params["survey_filter"] = survey_filter
+            result = requests.get("https://uclapi.com/workspaces/surveys", params=params).json()
+            if not result["ok"]:
+                raise BadResponseError
+            surveys = []
+            for survey in result["data"]:
+                surveys.append(Survey(survey))
+            return surveys
+
+        class sensors:
+
+            def __init__(self, client):
+                self.client = client
+
+                Averages = namedtuple('Averages', 'time')
+                self.averages = Averages(self.averages_time)
+
+            def __call__(self, survey_id, return_states="false"):
+                params = {}
+                params["token"] = self.client.token
+                params["survey_id"] = survey_id
+                params["return_states"] = return_states
+                result = requests.get("https://uclapi.com/workspaces/sensors", params=params).json()
+                if not result["ok"]:
+                    raise BadResponseError
+                maps = []
+                for map in result["maps"]:
+                    maps.append(Map(map))
+                return maps
+
+            def averages_time(self, days, **kwargs):
+                params = kwargs
+                params["days"] = days
+                params["token"] = self.client.token
+                result = requests.get("https://uclapi.com/workspaces/sensors/averages/time", params=params).json()
+                if not result["ok"]:
+                    raise BadResponseError
+                surveys = []
+                for survey in result["surveys"]:
+                    surveys.append(Averages(survey))
+                return surveys
+
+            def lastupdated(self, survey_id):
+                params = {
+                    "survey_id": survey_id,
+                    "token": self.client.token
+                }
+                result = requests.get("https://uclapi.com/workspaces/sensors/lastupdated")
+                if not result["ok"]:
+                    raise BadResponseError
+                Result = namedtuple("Result", "last_updated survey_id")
+                return Result(result["last_updated"], result["survey_id"])
+
+            
 
 class Bookings:
     def __init__(self, result, token):
@@ -223,3 +290,61 @@ class Desktop:
         location["coordinates"] = {"lat": desktop["location"]["latitude"], "lng": desktop["location"]["longitude"]}
         location["address"] = desktop["location"]["address"].split(", ") + [desktop["location"]["postcode"]]
         self.location = Location(location)
+
+class Survey:
+    def __init__(self, survey):
+        self.id = survey["id"]
+        self.name = survey["name"]
+        self.active = survey["active"]
+        self.start_time = survey["start_time"]
+        self.end_time = survey["end_time"]
+        self.staff_survey = survey["staff_survey"]
+        self.location = Location(survey["location"])
+        self.maps = []
+        for map in survey["maps"]:
+            self.maps.append(Maps(map))
+
+class Map:
+    def __init__(self, map):
+        self.id = map["id"]
+        self.name = map["name"]
+        self.image_id = map["image_id"]
+        self.sensors = {}
+        if "sensors" in map:
+            for sensor_id in map["sensors"]:
+                self.sensors[sensor_id] = Sensor(sensor_id, map["sensors"][sensor_id])
+
+class Sensor:
+    def __init__(self, id, sensor):
+        self.id = id
+        self.description_2 = sensor["description_2"]
+        self.floor = sensor["floor"]
+        self.y_pos = sensor["y_pos"]
+        self.description_3 = sensor["description_3"]
+        self.device_type = sensor["device_type"]
+        self.host_address = sensor["host_address"]
+        self.building_name = sensor["building_name"]
+        self.room_description = sensor["room_description"]
+        self.last_trigger_type = sensor["last_trigger_type"]
+        self.survey_id = sensor["survey_id"]
+        self.room_type = sensor["room_type"]
+        self.room_name = sensor["room_name"]
+        self.room_id = sensor["room_id"]
+        self.location = sensor["location"]
+        self.survey_device_id = sensor["survey_device_id"]
+        self.share_id = sensor["share_id"]
+        self.x_pos = sensor["x_pos"]
+        self.description_1 = sensor["description_1"]
+        self.hardware_id = sensor["hardware_id"]
+        self.pir_address = sensor["pir_address"]
+        self.last_trigger_timestamp = sensor["last_trigger_timestamp"]
+        self.occupied = sensor["occupied"]
+
+class Averages:
+    def __init__(self, averages):
+        self.survey_id = averages["survey_id"]
+        self.name = averages["name"]
+        self.averages = {}
+        Sensor_status = namedtuple("Sensor_status", "sensors_absent sensors_total sensors_occupied")
+        for average in averages["averages"]:
+            self.averages[average] = Sensor_status(averages["averages"][average]["sensors_absent"], averages["averages"][average]["sensors_total"], averages["averages"][average]["sensors_occupied"])
